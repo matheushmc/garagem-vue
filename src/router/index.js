9a5d1fd966e652cc14c03b1c885c082f5fe2ab2f
matheushmc <matheus20carvalhoteteu@gmail.com>
2024-04-01 11:30:53 -0300

    criação do vue acessorio, cor e marca

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,10 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 import CategoriaView from "../views/CategoriaView.vue";
+import AcessorioView from "../views/AcessorioView.vue";
+import CorView from "../views/CategoriaView.vue"
+import MarcaView from "../views/MarcaView.vue"
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +18,21 @@ const router = createRouter({
       path: "/categorias",
       name: "categorias",
       component: CategoriaView,
+    },
+    {
+      path: "/acessorios",
+      name: "acessorios",
+      component: AcessorioView,
+    },
+    {
+      path: "/cores",
+      name: "cores",
+      component: CorView,
+    },
+    {
+      path: "/marca",
+      name: "marca",
+      component: MarcaView,
     },
   ],
 });
